25db901b4c161a7b1a4dd90194f908ce9aa4fa97
Mario Medrano Maldonado <mariomedranomaldonado@gmail.com>
2018-11-14 23:59:33 -0400

    feat: Package ready to be published

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -11,7 +11,8 @@ module.exports = {
    entry: `${INPUT_PATH}/index.js`,
    output: {
       path: OUTPUT_PATH,
-      filename: 'index.js'
+      filename: 'index.js',
+      libraryTarget: 'commonjs2' // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
    },
    module: {
       rules: [
@@ -44,5 +45,8 @@ module.exports = {
       new ExtractTextPlugin({
          filename: 'style.css'
       })
-   ]
+   ],
+   externals: {
+      react: 'commonjs react' // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
+   }
 };
